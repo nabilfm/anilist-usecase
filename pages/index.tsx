f36273416a2010800bcabb47d9bba7466dcdab51
nabilfm <nabilfm39@gmail.com>
2022-05-22 18:19:11 +0700

    adjustment lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,16 @@
-import {useEffect} from "react";
+import { useEffect } from 'react'
 
 export default function Home() {
   useEffect(() => {
     try {
-      window.flutter_inappwebview.callHandler('enableButton')
-          .then(function (result: any) {
-            // print to the console the data coming
-            // from the Flutter side.
-            console.log(JSON.stringify(result));
-          });
+      window.flutter_inappwebview.callHandler('enableButton').then(function (result: any) {
+        // print to the console the data coming
+        // from the Flutter side.
+        console.log(JSON.stringify(result))
+      })
     } catch (e) {
-      // @ts-ignore
       console.warn(`error: ${e.toString()}`)
     }
   }, [])
-  return (
-    <div>
-      test
-    </div>
-  )
+  return <div>test</div>
 }
